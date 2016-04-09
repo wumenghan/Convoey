8e69fba0ed83db422909b434e9c89aeb1acfb742
wumenghan <wu784@purdue.edu>
2016-04-09 14:17:38 -0400

    add tracking location

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from tornado import ioloop, web, gen, concurrent
 import logging, sys, os, json, time
 
+PORT	   = 8010
+URL_PREFIX = r'/%02d'%(PORT % 100)
+DEBUG	   = True
 
 
 
@@ -8,22 +11,20 @@ class MainHandler(web.RequestHandler):
 	def get(self):
 		self.render("index.html")
 
-class NewHandler(web.RequestHandler):
-	def post(self):
 
-
-
-class UpdateHandler(web.RequestHandler):
-	@gen.coroutine
-	def post(self):
-
+def main():
+	static_path = os.path.join(os.path.dirname(__file__), "static")
+	app = web.Application(
+		[ ((URL_PREFIX+r"/"),			 MainHandler),
+		  ((URL_PREFIX+r'/static/(.*)'), web.StaticFileHandler, {'path': static_path}),
+		  ],
+		template_path = os.path.join(os.path.dirname(__file__), "templates"),
+		debug		  = DEBUG,
+	)
+	
+	app.listen(address="127.0.0.1", port=PORT)
+	sys.stderr.write("Starting server at http://serverhost:%d\n"%PORT)
+	ioloop.IOLoop.instance().start()
 
 if __name__ == "__main__":
-	app = tornado.web.Application(
-		[	(r"/",		 MainHandler),
-			(r"/update", UpdateHandler)
-
-		],
-
-
-		)
+	main()
--- static/main.js
+++ static/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function() { 
 	getLocation();
-
 });
 
 
@@ -8,20 +7,22 @@ function getLocation() {
 
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(initMap, error);
+		//var watchID = navigator.geolocation.watchPosition(showPosition);
+		//navigator.geolocation.watchPosition(initMap,error, optn);
 
 	} else{
 		alert("Geolocation is not supported by this browser");
 	}
 }
 
-
-function initMap(position) {
-	
+function initMap(position) {	
 	var LatLng = {lat:position.coords.latitude, lng:position.coords.longitude};
 
-	var map = new google.maps.Map(document.getElementById("map"), {
-		zoom:4,
-		center: LatLng
+	console.log(position.coords.latitude, position.coords.longitude)
+	var put_map = document.getElementById("map"); 
+	var map = new google.maps.Map(put_map, {
+		center: LatLng,
+		zoom:18
 	});
 
 	var marker = new google.maps.Marker({	
@@ -29,9 +30,27 @@ function initMap(position) {
 		map:map
 	});
 
+	// update center and marker
+	if(navigator.geolocation){
+		var optn = {
+			enableHighAccurarcy: true,
+			timeout            : Infinity,
+			maximumAge         : 0
+		};
+
+		navigator.geolocation.watchPosition(function(position){
+			var LatLng = {lat:position.coords.latitude, lng:position.coords.longitude};
+			map.setCenter(LatLng);
+			marker.setPosition(LatLng);
+			// use ajax to send location and username to server
+		},error,optn
+		);	
+	
+	}
+
 
 }
 
 function error(err){
-	alert(err)
+	alert(err);
 }
