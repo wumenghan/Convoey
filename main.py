a124f9abce325c2c66d3fa2846a3ab97fd623e75
wumenghan <wu784@purdue.edu>
2016-04-13 22:39:24 -0400

    show differnt user same time

--- main.py
+++ main.py
@@ -5,17 +5,52 @@ PORT	   = 8010
 URL_PREFIX = r'/%02d'%(PORT % 100)
 DEBUG	   = True
 
+g_location_infos = {} 
+g_waiters = set()
 
 
 class MainHandler(web.RequestHandler):
 	def get(self):
 		self.render("index.html")
 
+class NewHandler(web.RequestHandler):
+	def post(self):
+		location = self.get_argument("locations")
+		user = self.get_argument("user")
+		g_location_infos[user] = json.loads(location)
+		print type(json.loads(location)) 
+		logging.info("Sending new location to %r listeners", len(g_waiters))
+
+		for future in g_waiters:
+			future.set_result(g_location_infos)
+
+		g_waiters.clear()
+	
+	
+class UpdateHandler(web.RequestHandler):
+	@gen.coroutine
+	def post(self):
+		num_seen = int(self.get_argument("num_seen", 0))
+		print num_seen
+		print len(g_location_infos)
+		if num_seen == len(g_location_infos):
+			self._future = concurrent.Future()
+			g_waiters.add(self._future)
+			yield self._future
+
+		if not self.request.connection.stream.closed():
+			self.write({"locations":g_location_infos})
+
+	def on_connection_close(self):
+		g_waiters.remove(self._future)
+		self._future.set_result([])
 
 def main():
 	static_path = os.path.join(os.path.dirname(__file__), "static")
 	app = web.Application(
 		[ ((URL_PREFIX+r"/"),			 MainHandler),
+		  ((URL_PREFIX+r"/new"),         NewHandler),
+		  ((URL_PREFIX+r"/update"),      UpdateHandler),
 		  ((URL_PREFIX+r'/static/(.*)'), web.StaticFileHandler, {'path': static_path}),
 		  ],
 		template_path = os.path.join(os.path.dirname(__file__), "templates"),
--- static/main.js
+++ static/main.js
@@ -1,5 +1,7 @@
+var g_num_seen = 0;
 $(document).ready(function() { 
 	getLocation();
+	poll();
 });
 
 
@@ -20,15 +22,15 @@ function initMap(position) {
 
 	console.log(position.coords.latitude, position.coords.longitude)
 	var put_map = document.getElementById("map"); 
-	var map = new google.maps.Map(put_map, {
+	window.map = new google.maps.Map(put_map, {
 		center: LatLng,
 		zoom:18
 	});
 
-	var marker = new google.maps.Marker({	
-		position: LatLng,
-		map:map
-	});
+	//var marker = new google.maps.Marker({	
+	//	position: LatLng,
+	//	map:map,
+	//});
 
 	// update center and marker
 	if(navigator.geolocation){
@@ -37,19 +39,54 @@ function initMap(position) {
 			timeout            : Infinity,
 			maximumAge         : 0
 		};
-
+		var user = prompt("Enter user name");
 		navigator.geolocation.watchPosition(function(position){
 			var LatLng = {lat:position.coords.latitude, lng:position.coords.longitude};
+			var position = JSON.stringify(LatLng);
 			map.setCenter(LatLng);
-			marker.setPosition(LatLng);
+			//marker.setPosition(LatLng);
 			// use ajax to send location and username to server
+			$.ajax({
+				url:"/10/new",
+				type:"POST",
+				data:{locations:position, user:user}, // user name will be changed in the future
+				success: function(data, text_status, jq_xhr){
+					console.log("success")	
+				},
+				error: function(jq_xhr, text_status, error_thrown) {
+					console.log("ERROR POSTING NEW MESSAGE:", error_thrown);
+				}
+			});	
 		},error,optn
 		);	
-	
 	}
+}
+
+function poll(){
+	$.ajax({
+		url:"/10/update",
+		type:"POST",
+		data:{num_seen:g_num_seen},
+		success: function(data, text_status, jq_xhr) {
+			var i = 0;
+			$.each(data.locations, function(key, val) {
+				var marker = new google.maps.Marker({
+					position:val,
+					label:key,
+					map:map
+				});
+
+				i++;
+			})
+		console.log(i);
+		g_num_seen = i;
+		poll();	
+		}
+	});
 
 
 }
+
 
 function error(err){
 	alert(err);
